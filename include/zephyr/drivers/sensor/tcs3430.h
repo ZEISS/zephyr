1892348121553837461e715ca964a0197f405330
Severin Maher <severin.maher@zeiss.com>
2025-06-10 10:56:51 +0200

    sensor: add tcs3430 driver
    Adds sensor driver for AMS TCS3430.
    The driver currently uses continuous
    readout mode without interrupts.
    
    Co-authored-by: Florian Guhl <florian.guhl@zeiss.com>
    Signed-off-by: Severin Maher <severin.maher@zeiss.com>

---
+++ include/zephyr/drivers/sensor/tcs3430.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2023 Google LLC
+ * SPDX-FileCopyrightText: Copyright (c) 2025 Carl Zeiss Meditec AG
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DRIVERS_SENSOR_TCS3430_H_
+#define ZEPHYR_INCLUDE_DRIVERS_SENSOR_TCS3430_H_
+
+#include <zephyr/drivers/sensor.h>
+
+enum sensor_attribute_tcs3430 {
+	/** XYZB Integration Cycles */
+	SENSOR_ATTR_TCS3430_INTEGRATION_CYCLES = SENSOR_ATTR_PRIV_START,
+	/** Gain setting */
+	SENSOR_ATTR_TCS3430_GAIN,
+};
+
+#endif /* ZEPHYR_INCLUDE_DRIVERS_SENSOR_TCS3430_H_ */
